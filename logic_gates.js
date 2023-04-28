de94cf108a7520b016eaeeb0a798c39943ebc5ba
Ved Vekhande <vedvekhande7274@gmail.com>
2023-04-28 23:00:01 +0530

    updated number of inputs

--- logic_gates.js
+++ logic_gates.js
@@ -1,58 +1,153 @@
-$(document).ready(function() {
-    // Define logic gates
-    function notGate(input) {
-        var output = [];
-        for (var i = 0; i < input.length; i++) {
-            output.push(input[i] == 0 ? 1 : 0);
-        }
-        if (output.length < input.length) {
-            for (var i = output.length; i < input.length; i++) {
-                output.push(0);
-            }
-        }
-        return output;
-    }
-    
+var inputWaveforms = []; // array to store input waveforms
 
-    function xorGate(input1, input2) {
-        var output = [];
-        for (var i = 0; i < input1.length; i++) {
-            output.push(input1[i] ^ input2[i]);
-        }
-        if (output.length < input1.length) {
-            for (var i = output.length; i < input1.length; i++) {
-                output.push(0);
-            }
-        }
-        return output;
-    }
-    
-    function norGate(input1, input2) {
-        var output = [];
-        for (var i = 0; i < input1.length; i++) {
-            output.push((input1[i] | input2[i]) == 0 ? 1 : 0);
-        }
-        if (output.length < input1.length) {
-            for (var i = output.length; i < input1.length; i++) {
-                output.push(0);
-            }
-        }
-        return output;
-    }
-    
+// add waveform to inputWaveforms array
+function addWaveform() {
+    var waveformInput = document.getElementById("waveform-input").value;
+    var waveform = waveformInput.split(",").map(Number);
+    inputWaveforms.push(waveform);
+    updateCharts();
+}
 
-    // Handle waveform selection and user input
-    $('#waveform, #submitBtn').on('click', function() {
-        var selected = $('#waveform').val();
-        var userInput = $('#userInput').val();
-        var inputWaveform = userInput ? userInput.split('').map(Number) : waveforms[selected];
-        var notOutput = notGate(inputWaveform);
-        var xorOutput = xorGate(inputWaveform, notOutput);
-        var norOutput = norGate(inputWaveform, notOutput);
+// update input and output charts
+function updateCharts() {
+    var gateSelect = document.getElementById("gate-select");
+    var selectedGate = gateSelect.options[gateSelect.selectedIndex].value;
 
-        // Display output waveforms
-        $('#output').html('<h2>NOT Gate Output:</h2><p>' + notOutput.join(', ') + '</p>'
-                         + '<h2>XOR Gate Output:</h2><p>' + xorOutput.join(', ') + '</p>'
-                         + '<h2>NOR Gate Output:</h2><p>' + norOutput.join(', ') + '</p>');
-    });
+    var inputLabels = [];
+    var inputDatasets = [];
+    for (var i = 0; i < inputWaveforms.length; i++) {
+        var label = "Input " + (i + 1);
+        var dataset = {
+            label: label,
+            data: inputWaveforms[i],
+            borderColor: "rgba(0, 0, 255, 1)",
+            fill: false,
+            // borderColor: 'blue',
+    borderWidth: 1
+        };
+        inputLabels.push(label);
+        inputDatasets.push(dataset);
+    }
+
+    var outputLabels = [];
+    var outputData = [];
+    if (selectedGate == "not") {
+        outputLabels = inputLabels;
+        for (var i = 0; i < inputWaveforms.length; i++) {
+            var output = notGate(inputWaveforms[i]);
+            outputData.push(output);
+        }
+    } else {
+        var waveform1 = inputWaveforms[0];
+        var waveform2 = inputWaveforms[1];
+        var label1 = "Input 1";
+        var label2 = "Input 2";
+        // if (inputWaveforms.length > 2) {
+        //     waveform2 = inputWaveforms[2];
+        //     label2 = "Input 3";
+        // }
+        outputLabels.push("Output");
+        if (selectedGate == "xor") {
+            var output = xorGate(waveform1, waveform2);
+            outputData.push(output);
+        } else if (selectedGate == "nor") {
+            var output = norGate(waveform1, waveform2);
+            outputData.push(output);
+        }
+        inputLabels = [label1, label2];
+        inputDatasets = [
+            {
+                label: label1,
+                data: waveform1,
+                borderColor: "rgba(0, 0, 255, 1)",
+                fill: false
+                ,
+},
+{
+label: label2,
+data: waveform2,
+borderColor: "rgba(255, 0, 0, 1)",
+fill: false
+}
+];
+}
+// update input chart
+var inputChartContext = document.getElementById("input-chart").getContext("2d");
+var inputChart = new Chart(inputChartContext, {
+    type: "line",
+    data: {
+        labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17" , "18", "19"],
+        datasets: inputDatasets
+    },
+    options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: "Input Waveforms"
+        }
+    }
 });
+
+// update output chart
+var outputChartContext = document.getElementById("output-chart").getContext("2d");
+var outputChart = new Chart(outputChartContext, {
+    type: "line",
+    data: {
+        labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17" , "18", "19"],
+        datasets: [
+            {
+                label: "Output",
+                data: outputData[0],
+                borderColor: "rgba(0, 255, 0, 1)",
+                fill: false
+            }
+        ]
+    },
+    options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: "Gate Output"
+        }
+    }
+});
+}
+
+// NOT gate function
+function notGate(waveform) {
+var output = [];
+for (var i = 0; i < waveform.length; i++) {
+    if (waveform[i] == 0) {
+        output.push(1);
+    } else {
+        output.push(0);
+    }
+}
+return output;
+}
+
+// XOR gate function
+function xorGate(waveform1, waveform2) {
+var output = [];
+for (var i = 0; i < waveform1.length; i++) {
+    if (waveform1[i] == waveform2[i]) {
+        output.push(0);
+    } else {
+        output.push(1);
+    }
+}
+return output;
+}
+
+// NOR gate function
+function norGate(waveform1, waveform2) {
+var output = [];
+for (var i = 0; i < waveform1.length; i++) {
+    if (waveform1[i] == 0 && waveform2[i] == 0) {
+        output.push(1);
+    } else {
+        output.push(0);
+    }
+}
+return output;
+}
