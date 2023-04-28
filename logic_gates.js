1f463ce95960200a5f83c57b57357bc6f55a6816
Ved Vekhande <vedvekhande7274@gmail.com>
2023-04-28 21:21:25 +0530

    updated logic gate

--- logic_gates.js
+++ logic_gates.js
@@ -1,40 +1,51 @@
 $(document).ready(function() {
-    // Define input waveforms
-    var waveforms = {
-        "waveform1": [0, 1, 0, 1, 0, 1],
-        "waveform2": [1, 0, 1, 0, 1, 0],
-        "waveform3": [0, 0, 1, 1, 0, 0],
-    };
-
     // Define logic gates
     function notGate(input) {
         var output = [];
         for (var i = 0; i < input.length; i++) {
             output.push(input[i] == 0 ? 1 : 0);
         }
+        if (output.length < input.length) {
+            for (var i = output.length; i < input.length; i++) {
+                output.push(0);
+            }
+        }
         return output;
     }
+    
 
     function xorGate(input1, input2) {
         var output = [];
         for (var i = 0; i < input1.length; i++) {
             output.push(input1[i] ^ input2[i]);
         }
-        return output;
-    }
-
-    function norGate(input1, input2) {
-        var output = [];
-        for (var i = 0; i < input1.length; i++) {
-            output.push(!(input1[i] || input2[i]) ? 1 : 0);
+        if (output.length < input1.length) {
+            for (var i = output.length; i < input1.length; i++) {
+                output.push(0);
+            }
         }
         return output;
     }
+    
+    function norGate(input1, input2) {
+        var output = [];
+        for (var i = 0; i < input1.length; i++) {
+            output.push((input1[i] | input2[i]) == 0 ? 1 : 0);
+        }
+        if (output.length < input1.length) {
+            for (var i = output.length; i < input1.length; i++) {
+                output.push(0);
+            }
+        }
+        return output;
+    }
+    
 
-    // Handle waveform selection
-    $('#waveform').on('change', function() {
-        var selected = $(this).val();
-        var inputWaveform = waveforms[selected];
+    // Handle waveform selection and user input
+    $('#waveform, #submitBtn').on('click', function() {
+        var selected = $('#waveform').val();
+        var userInput = $('#userInput').val();
+        var inputWaveform = userInput ? userInput.split('').map(Number) : waveforms[selected];
         var notOutput = notGate(inputWaveform);
         var xorOutput = xorGate(inputWaveform, notOutput);
         var norOutput = norGate(inputWaveform, notOutput);
